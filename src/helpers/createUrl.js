d359e06e8187d1b6bdfad420587702c92072c3fe
Kristen Sullivan <you@example.com>
2018-07-31 20:34:08 -0400

    changed http to https in createURL

--- src/helpers/createUrl.js
+++ src/helpers/createUrl.js
@@ -1,5 +1,5 @@
 export const createUrl = (type, lat, long) => {
-  const uri = 'http://api.wunderground.com/api/';
+  const uri = 'https://api.wunderground.com/api/';
   const key = 'bcdeab02bf65e45e';
   const url = uri + key + '/' + type + '/q/' + lat + ',' + long + '.json';
   console.log('data from getWeather function: ' + url);
